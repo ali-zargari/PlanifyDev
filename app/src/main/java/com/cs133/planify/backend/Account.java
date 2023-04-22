c8ddd9801fd613508d4818de963e152d344f72f7
tri tran <trittp123@gmail.com>
2023-04-22 02:31:00 -0700

    added a toMap function used for database operations, removed password field as no longer necessary since mAuth in firebase does that

--- app/src/main/java/com/cs133/planify/backend/Account.java
+++ app/src/main/java/com/cs133/planify/backend/Account.java
@@ -1,11 +1,16 @@
 
 package com.cs133.planify.backend;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Account {
     private ArrayList<Calendar> sharedCalendars= new ArrayList<Calendar>();
-    private String password;
     private String name;
     private Calendar mainCalendar;
+
+
 
 
     public Account(){
@@ -14,10 +19,23 @@ public class Account {
 
     }
 
-    public Account(String name, String password, Calendar mainCalendar) {
+    public Account(String name, Calendar mainCalendar) {
         this.name = name;
-        this.password = password;
         this.mainCalendar = mainCalendar;
+    }
+
+    public static Map<String, Object> toMap(Account thisAccount) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("mainCalendar", thisAccount.getMainCalendar());
+        result.put("calendars", thisAccount.getCalendars());
+        result.put("name", thisAccount.getName());
+
+        return result;
+    }
+    //function to be called to update the account calendar values every time a change is made on the database
+    public void update(Map<String, Object> map){
+        setMainCalendar((Calendar) map.get("mainCalendar"));
+        setCalendars((ArrayList<Calendar>) map.get("calendars"));
     }
 
     public ArrayList<Calendar> getCalendars() {
@@ -28,9 +46,7 @@ public class Account {
         this.sharedCalendars = calendars;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+
 
     public void setName(String name) {
         this.name = name;
@@ -46,9 +62,7 @@ public class Account {
         this.mainCalendar = mainCalendar;
     }
 
-    public String getPassword() {
-        return password;
-    }
+
 
     public String getName() {
         return name;
