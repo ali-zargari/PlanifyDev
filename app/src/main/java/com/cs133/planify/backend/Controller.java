2b1e45f06494367bf2f389f4c79f7f853881c7a0
tri tran <trittp123@gmail.com>
2023-04-26 16:53:47 -0700

    added testlogin class to controller and created testing suite for database operations with test initliaize and login methods (does not work yet)

--- app/src/main/java/com/cs133/planify/backend/Controller.java
+++ app/src/main/java/com/cs133/planify/backend/Controller.java
@@ -1,9 +1,17 @@
 package com.cs133.planify.backend;
 
+import android.content.Intent;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 
 import java.util.ArrayList;
 
+import com.cs133.planify.frontend.login.Login;
+import com.cs133.planify.frontend.main.Main;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -29,7 +37,7 @@ public class Controller {
         this.ThisAccount= newAcc;
     }
     //intialize controller must be run the first time an account as created, will create a child in database with information on user Account
-    public void initalizeDatabase(){
+    public Boolean initalizeDatabase(){
         // uses mAuth to get user ID and creates a new mainCalendar object with ID empty calendar
         userAcc= new Account(mAuth.getUid(),new Calendar("mainCalendar"));
         //intializes database reference
@@ -37,15 +45,18 @@ public class Controller {
         DBref= userDB.getReference();
         // craetes map and uses it to initialize data locales
         DBref.updateChildren(Account.toMap(userAcc));
+        return true;
     }
-    public void updateDB(){
+    public Boolean updateDB(){
         //updates all children values in database with current values in the calendar
         DBref.updateChildren(Account.toMap(userAcc));
+        return true;
     }
 
     public void updateLocal(){
 
     }
 
-
 }
+
+
---
+++ app/src/test/java/com/cs133/planify/DatabaseTest.java
@@ -0,0 +1,42 @@
+package com.cs133.planify;
+import com.cs133.planify.backend.Controller;
+import com.cs133.planify.backend.Account;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class DatabaseTest {
+    public String TestEmail= "trittp123@gmail.com";
+    public String TestPassword= "testpassword123";
+    private FirebaseAuth mauth =FirebaseAuth.getInstance();
+
+
+    public Controller TestController = new Controller();
+
+    public boolean initializeTestLogin(){
+        mauth.signInWithEmailAndPassword(TestEmail, TestPassword);
+        return true;
+    }
+    @Test
+    public void unitTestTestLogin(){
+        assert(initializeTestLogin());
+    }
+
+    @Test
+    public void unitTestinitializeuserDB() throws Exception{
+        if(!initializeTestLogin()){
+            throw new Exception("login failed");
+        }
+        boolean success= TestController.initalizeDatabase();
+        assert(success);
+    }
+    @Test
+    public void unitTestUpdateDB() throws Exception{
+        if(!initializeTestLogin()){
+            throw new Exception("login failed");
+        }
+        boolean success= TestController.initalizeDatabase();
+        assert(success);
+    }
+}
