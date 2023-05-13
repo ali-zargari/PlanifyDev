143d4d73d178d9512b88848a1d68f02bab953741
Tri Tran <Trittp123@gmail.com>
2023-05-13 15:42:27 -0700

    created fragments for creating tasks and events

---
+++ app/src/main/java/com/cs133/planify/frontend/CreateTask/CreateTaskFragment.java
@@ -0,0 +1,37 @@
+package com.cs133.planify.frontend.CreateTask;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import com.cs133.planify.backend.Controller;
+import com.cs133.planify.backend.Event;
+import com.cs133.planify.backend.Globals;
+import com.cs133.planify.backend.Task;
+
+import java.util.ArrayList;
+
+public class CreateTaskFragment extends Fragment {
+    Controller mController;
+    ArrayList<Task> TaskList;
+    ArrayList<Event> EventList;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Controller mController= Globals.getController();
+        EventList= mController.userAcc.getEvents();
+        TaskList= mController.userAcc.getTasks();
+
+
+    }
+
+    public void addTaskFromUI(Task newTask) throws Exception{
+        Globals.mController.userAcc.addTask(newTask);
+        Globals.mController.updateDB();
+    }
+    public void shareTaskFromUI(Task newTask, String Email) throws Exception{
+        Globals.mController.shareTask(newTask,Email);
+        Globals.mController.updateDB();
+    }
+}
